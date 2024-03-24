da72b5bce272f10e5b90e7721f47ae2beff265bf
meron6 <khozameron5@gmail.com>
2024-03-24 10:11:59 -0700

    0-fizzbuzz.py

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -15,18 +15,18 @@ def fizzbuzz(n):
     if n < 1:
         return
 
-    tmp_result = []
+    result = []
     for i in range(1, n + 1):
-        if (i % 3 == 0) and (i % 5 == 0):
-            tmp_result.append("FizzBuzz")
-        elif (i % 3 == 0):
-            tmp_result.append("Fizz")
-        elif (i % 5 == 0):
-            tmp_result.append("Buzz")
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
         else:
-            tmp_result.append(str(i))
-    print(" ".join(tmp_result))
-
+            result.append(str(i))
+    
+    print(" ".join(result))
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
